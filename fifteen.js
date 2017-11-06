113a2559cef77ddb517726940f1277a503a03ccf
DanChrissy <32615517+DanChrissy@users.noreply.github.com>
2017-11-06 00:53:41 -0800

    Adding move and moveable functions

--- fifteen.js
+++ fifteen.js
@@ -1,10 +1,32 @@
 window.onload = function (){
+
 	allPuzzle();
-	/*var shuffleButton = document.getElementById("shufflebutton");
-	shufflebutton.onclick = function (){
-		var puzzle = document.getElementById("puzzlearea");
-		shuffle(puzzle);
-	}*/
+
+	var pieces = document.getElementById("puzzlearea").querySelectorAll("div");
+
+	var shuffleButton = document.getElementById("shufflebutton");
+	var area = document.getElementById("puzzlearea");
+	shuffleButton.onclick = function() {
+        shuffle(pieces);
+    };
+
+	for (s = 0; s <= pieces.length; i++){
+		pieces[s].onmouseover = function(){
+			if moveable(this){
+				this.setAttribute("class", "puzzlepiece moveablepiece");
+			};
+		};
+
+		pieces[s].onmouseleave = function(){
+			this.setAttribute("class", "#puzzlearea puzzlepiece");
+		};
+
+		pieces[s].onclick = function(){
+			if (this.hasattribute("moveablepiece")){
+				move(this,true);
+			}
+		};
+	};
 }
 
 function allPuzzle (){
@@ -17,16 +39,6 @@ function allPuzzle (){
 		numbers[i].setAttribute("class", "#puzzlearea puzzlepiece");
 		numbers[i].style.marginLeft = countL + "px";
 		numbers[i].style.marginTop = countT + "px";
-
-		for (var t = 0; t <= numbers.length; i++) {
-			var idTop = countL.toString();
-			var idLeft = countT.toString();
-			var idTile = idTop + "_" + idLeft;
-
-			numbers[t].setAttribute("id", "idTile");
-		};
-
-
 	}
 
 	for (var i = 0; i <= 3; i++){
@@ -71,7 +83,7 @@ function allPuzzle (){
 	};countL = 0;
 	countT = countT + 100;
 
-	for (var i = 12; i <= 14; i++){
+	for (var i = 12; i <= 15; i++){
 		for(var p = 12; p <=14; p++){
 			numbers[p].style.backgroundPosition = countB + "px -300px";
 			countB -= 100;
@@ -83,8 +95,105 @@ function allPuzzle (){
 			countL += 100;			
 		};	
 	};countL = 0;
-
 }
+
+function moveable(tile){
+	let pieceTop = tile.style.top;
+	let pieceLeft = tile.style.left;
+
+	for (var i = 0; i <= numbers.length; i++) {
+		if(numbers[i].style.backgroundImage = "none"){
+			let emptyTop = numbers[i].style.top ;
+			let emptyLeft = numbers[i].style.left;
+		};
+	};
+
+	if (parseInt(pieceTop) + 100 == parseInt(emptyTop) && parseInt(pieceLeft) == parseInt(emptyLeft) ||
+	    parseInt(pieceLeft) + 100 == parseInt(emptyLeft) && parseInt(pieceTop) == parseInt(emptyTop) ||
+	    parseInt(pieceTop) - 100 == parseInt(emptyTop) && parseInt(pieceLeft) == parseInt(emptyLeft) ||
+	    parseInt(pieceLeft) - 100 == parseInt(emptyLeft) && parseInt(pieceTop) == parseInt(emptyTop)){
+
+		return tile;
+	};
+}
+
+/*Tiles can either have margintop to -100 or +100 or marginleft to -100 or +100*/
+
+function move(tile, movement){
+	let speed = setInterval(frame, 5);
+	let currentTop = tile.style.top;
+	let posDown = currentTop + 100
+	let posUp = currentTop - 100;
+
+	let currentLeft = tile.style.left;
+	let posLeft = currentLeft + 100;
+	let posRight = currentLeft -
+
+	if (movement == true){
+		function frame()
+		{
+			if (currentTop == posDown){
+				clearInterval(speed);
+			}else{
+				currentTop++;
+				tile.style.top = currentTop + "px";
+			}
+
+			if (currentLeft == posLeft){
+				clearInterval(speed);
+			}else{
+				currentLeft++;
+				tile.style.left = currentLeft + "px";
+			}
+
+			if (currentTop == posUp){
+				clearInterval(speed);
+			}else{
+				currentTop--;
+				tile.style.top = currentTop + "px";
+			}
+
+			if (currentLeft == posLeft){
+				clearInterval(speed);
+			}else{
+				currentLeft--;
+				tile.style.left = currentLeft + "px";
+			}
+
+
+			if (currentTop == posDown || currentTop == posUp){
+				clearInterval(speed);
+				if (currentTop == posDown){
+					currentTop++;
+				}else {
+					currentTop--;
+				}tile.style.top = currentTop + "px";
+			}else if(currentLeft == posLeft || currentLeft == posRight){
+				if (currentLeft == posLeft){
+					currentLeft++;
+				}else{
+					currentLeft--;
+				}tile.style.left = currentLeft + "px";
+			};
+		};
+	}
+}
+
+function shuffle(tiles){
+	for (let b = 0; b < tiles.length; b++){
+		let random = Math.floor(Math.random() * tiles.length);
+		let tile = tiles.splice(random, 1);
+		move(tile[0], false);
+	};
+}
+
+
+
+
+
+
+
+
 
 
 
